a7225705c19dd9604476a8c6c9ad6e66fdee66e3
Sprixitite <91488389+Sprixitite@users.noreply.github.com>
2023-06-26 05:59:10 +0100

    I don't know dotnet cli is being a bitch

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+using PD2ModHelpLib;
+
+namespace PD2ModHelperCLI;
+
+public static class Program {
+
+    public static void Main(string[] args) {
+
+        // foreach ( string s in args ) { Console.WriteLine(s); }
+
+        Mod testmod = new Mod("testmod", "/home/rory/Projects/testmod");
+
+        testmod.AddMenu("mymenu", "blt_options", "mymenu_title", "mymenu_desc");
+        testmod.AddOption("mymenu", new MenuButton("mybtn", "mybtn_title", "mybtn_desc", "mybtn_callback"));
+
+    }
+
+}
